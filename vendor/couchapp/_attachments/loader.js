44fb756a92f9595c28236e9cdb75c5b16ecb923b
Mehdi El Fadil <m.ef@mango-is.com>
2011-08-01 21:13:39 +0200

    first commit

---
+++ vendor/couchapp/_attachments/loader.js
@@ -0,0 +1,16 @@
+
+function couchapp_load(scripts) {
+  for (var i=0; i < scripts.length; i++) {
+    document.write('<script src="'+scripts[i]+'"><\/script>')
+  };
+};
+
+couchapp_load([
+  "/_utils/script/sha1.js",
+  "/_utils/script/json2.js",
+  "vendor/couchapp/jquery-1.6.1.min.js",
+  "vendor/couchapp/jquery.couch.js",
+  "vendor/couchapp/jquery.couch.app.js",
+  "vendor/couchapp/jquery.couch.app.util.js",
+  "vendor/couchapp/jquery.csvIn.min.js"
+]);
